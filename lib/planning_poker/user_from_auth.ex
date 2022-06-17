dbed2e4c89befccd6002b8d8ca2cde8383743297
Christoph Geschwind <christophgeschwind@gmail.com>
2022-06-17 11:16:12 +0200

    remove unused code regarding password login

--- lib/planning_poker/user_from_auth.ex
+++ lib/planning_poker/user_from_auth.ex
@@ -9,16 +9,6 @@ defmodule PlanningPoker.UserFromAuth do
 
   alias Ueberauth.Auth
 
-  def find_or_create(%Auth{provider: :identity} = auth) do
-    case validate_pass(auth.credentials) do
-      :ok ->
-        {:ok, basic_info(auth)}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
-  end
-
   def find_or_create(%Auth{} = auth) do
     {:ok, basic_info(auth)}
   end
@@ -55,18 +45,4 @@ defmodule PlanningPoker.UserFromAuth do
       end
     end
   end
-
-  defp validate_pass(%{other: %{password: nil}}) do
-    {:error, "Password required"}
-  end
-
-  defp validate_pass(%{other: %{password: pw, password_confirmation: pw}}) do
-    :ok
-  end
-
-  defp validate_pass(%{other: %{password: _}}) do
-    {:error, "Passwords do not match"}
-  end
-
-  defp validate_pass(_), do: {:error, "Password Required"}
 end
