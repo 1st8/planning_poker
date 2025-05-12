654ab00ceb95ddb430da4a8be7f7e6780c44dc17
Christoph Geschwind <christoph.geschwind@tixxt.com>
2025-05-12 16:27:36 +0200

    Update theme settings and improve marker card styles in Magic Estimation component

--- lib/planning_poker_web/live/planning_session_live/magic_estimation_component.ex
+++ lib/planning_poker_web/live/planning_session_live/magic_estimation_component.ex
@@ -5,7 +5,7 @@ defmodule PlanningPokerWeb.PlanningSessionLive.MagicEstimationComponent do
   defp render_item(assigns) do
     ~H"""
     <%= if @item["type"] == "marker" do %>
-      <div class="marker-card bg-primary text-primary-content rounded-lg p-2 mb-2 cursor-move" data-id={@item["id"]}>
+      <div class="marker-card bg-accent text-accent-content rounded-lg p-2 mb-2 cursor-move" data-id={@item["id"]}>
         <div class="flex gap-4 items-center justify-center">⬇️<span class="font-bold text-lg"><%= @item["value"] %></span>⬇️</div>
       </div>
     <% else %>
