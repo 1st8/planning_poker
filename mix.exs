e954f7727d09ac0d20316ecb7157c9326e427c6a
Christoph Geschwind <christoph.geschwind@tixxt.com>
2025-11-04 11:44:23 +0100

    Release version 0.6.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlanningPoker.MixProject do
   def project do
     [
       app: :planning_poker,
-      version: "0.6.1",
+      version: "0.6.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
